149636ecac832e0b3de4a0da85a98817a17aa54c
henry <143163741+s4larie@users.noreply.github.com>
2025-11-24 12:17:57 +0100

    Add scripts to check balances/txns for some coins

--- check_dogechain.py
+++ check_dogechain.py
@@ -5,7 +5,7 @@ import sys
 import time
 from walletool import init_env
 
-def main():
+def main(): 
     ap = argparse.ArgumentParser()
     ap.add_argument('file', help='address file; one address per line')
     ap.add_argument('--ignore-empty', action='store_true')
